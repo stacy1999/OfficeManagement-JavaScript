f4c5cc918a357d61b4de932506610856641d7514
stacy1999 <anastasiapolinesi@gmail.com>
2021-02-26 15:57:40 +0000

    User Story 8

--- spec/officeSpec.js
+++ spec/officeSpec.js
@@ -4,17 +4,16 @@ describe('Office', function(){
   var office;
   var meetingRoom;
   beforeEach(function(){
-    office = new Office(meetingRoom);
+    office = new Office();
+    meetingRoom = new MeetingRoom('room for jello')
   });
   
-   it('has no rooms by default', function(){
+  it('has no rooms by default', function(){
     expect(office.getRooms()).toEqual([]);
-   });
+  });
 
   it('creates a meeting room', function(){
     office.createMeetingRoom('room for jello');
-    expect(office.getRooms()).toEqual([{ availability: true, roomName: 'room for jello' }]);
+    expect(office.getRooms()).toEqual([meetingRoom]);
   });
-
-  
 })
--- src/office.js
+++ src/office.js
@@ -2,14 +2,25 @@
 class Office {
   constructor(){
     this._rooms = [];
+    this._availableRooms = [];
   }
 
   getRooms() {
-    return this._rooms;
+   return this._rooms;
   };
 
   createMeetingRoom(meetingRoomName){
-    let newRoom = new MeetingRoom(meetingRoomName);
-    this._rooms.push({ availability: newRoom.availability, roomName: newRoom.roomName});
+    var room = new MeetingRoom(meetingRoomName);
+    this._rooms.push(room);
   };
+  
+  meetingRoomAvailable(){
+    for (let i = 0; i < this._rooms.length; i++){
+      if (this._rooms[i].availability === true) {
+        this._availableRooms.push(this._rooms[i]);
+      }
+    }
+    return this._availableRooms;
+  }
+  
 }
